bfa048178b2e1b92a7a4c98bfbbb9b0b5d0c8fd9
unknown <3122561752@qq.com>
2024-09-01 20:38:27 +0800

    feat(index):完成了header区域开发

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,60 @@
-import {Layout} from "antd";
+import {Button, Dropdown, Input, Layout, Menu, MenuProps, Space} from "antd";
+import {ItemType} from "antd/es/menu/interface";
+import {DownOutlined} from "@ant-design/icons";
 
-const navigationOptions = ["首页", "课程中心", "超级会员", "工具", "自学路线", ""]
+const selfItems: MenuProps = {
+    items: [
+        {key: "front", label: "前端自学路线"},
+        {key: "back", label: "后端"}
+    ]
+
+}
+const oneItems: MenuProps = {
+    items: [
+
+        {key: "front", label: "前端一对一"},
+        {key: "back", label: "后端一对一"},
+    ]
+}
+const menuItems: ItemType[] = [
+    {key: "index", label: <span>首页</span>},
+    {key: "course", label: <span>课程中心</span>},
+    {
+        key: "svip",
+        label: <div className={"flex items-center "}>
+            <img src="https://front.cdn.xdclass.net/images/vip_icon.webp" width={20} height={20}/>
+            <span>超级会员</span>,
+        </div>
+    },
+    {key: "tool", label: <span>工具</span>},
+    {
+        key: "selfStudy",
+        label: <Dropdown menu={selfItems}>
+            <span>自学路线<DownOutlined/></span>
+        </Dropdown>
+    },
+    {
+        key: "oneToOne",
+        label: <Dropdown menu={oneItems}>
+            <span>一对一辅导<DownOutlined/></span>
+        </Dropdown>
+    },
+    {
+        key: "search",
+        label: <Input.Search className={"w-36 translate-y-1/2"} placeholder="请输入搜索内容"/>
+    },
+    {
+        key: "active",
+        label: <Button type="primary">激活</Button>
+    },
+    {
+        key: "cloud",
+        label: <span>云服务器</span>
+    },
+
+
+]
+
 export const Index = () => {
     return (
         <Layout className={"w-full bg-transparent"}>
@@ -13,11 +67,15 @@ export const Index = () => {
                     {/*logo*/}
                     <img src="https://front.cdn.xdclass.net/images/logo.webp" className="w-[138px] h-[63px]"/>
                     {/* 导航*/}
-                    <div className={"flex justify-between items-center"}>
+                    {/*<div className={"flex justify-between items-center "}>*/}
 
-                    </div>
+                    {/*</div>*/}
+                    <Menu mode="horizontal" items={menuItems} className={"flex-1 flex  justify-between items-center"}/>
                     {/*登录注册*/}
-                    <div></div>
+                    <Space size="large">
+                        <span>登录</span>
+                        <Button type="primary" className={"hover:!bg-[#4D555D] bg-[#4D555D] "}>注册</Button>
+                    </Space>
                 </div>
             </Layout.Header>
 
