9dbf0f3a6b0f4d3d2fd775da4c6fef3ee55d5ef7
unknown <3122561752@qq.com>
2024-09-03 23:15:41 +0800

    feat(index):完成了注册弹窗

--- src/components/RegisterModel/index.tsx
+++ src/components/RegisterModel/index.tsx
@@ -1,5 +1,6 @@
 import {FC} from "react";
-import {Col, Divider, Modal, ModalProps, Row, Typography} from "antd";
+import {Button, Checkbox, Col, Divider, Input, Modal, ModalProps, Row, Typography} from "antd";
+import {CloseOutlined} from "@ant-design/icons";
 
 
 export const RegisterModel: FC<ModalProps> = (props) => {
@@ -21,7 +22,59 @@ export const RegisterModel: FC<ModalProps> = (props) => {
                             className="block mx-auto text-white tracking-[10px] font-bold text-xl ">让技术不在难学</Typography.Text>
                     </div>
                 </Col>
-                <Col span={15}>hh</Col>
+                <Col span={15}>
+                    <div className="px-[50px] pt-[36px] box-border ">
+                        <div className=" w-full h-[32px] flex justify-between ">
+                            <span className="abtext-[#404040] font-bold text-xl ">快速注册</span>
+                            <CloseOutlined className=" w-[20px] h-[20px]  "/>
+                        </div>
+                        <div>
+                            <form className="text-[#FFF]">
+                                <Input type="text" placeholder="请输入手机号或邮箱" className="mt-[20px]"/>
+                                <Input type="text" placeholder="请输入图片验证码" className="my-[20px]"/>
+                                <div
+                                    className="flex flex-row w-full border rounded-md border-solid border-[#e5e7eb] mb-[20px]">
+                                    <div className=" border-none">
+                                        <Input placeholder="请输入手机号或邮箱验证码" type="text"
+                                               className=" border-none"/>
+                                    </div>
+                                    <div>
+                                        <Button type="link">获取验证码</Button>
+                                    </div>
+                                </div>
+                                {/*<span*/}
+                                {/*    className="flex flex-row  justify-between border-[1px] border-solid border-[#d9d9d9] rounded-[5px] mb-[20px] w-full px-[5px]">*/}
+                                {/*    <Input type="text" placeholder="请输入手机号或邮箱验证码"*/}
+                                {/*           className=" text-[#FFFFFF]"/>*/}
+                                {/*    <span> <Button type="link">获取验证码</Button></span>*/}
+                                {/*</span>*/}
+
+                            </form>
+                            <div>
+                                <Checkbox className="mr-[20px] text-[15px]"/>
+                                <span className="text-[#000000D9] ">同意小滴课堂</span><a
+                                className="text-[#169BD5]">《隐私策略》</a>
+                            </div>
+                            <Button type="primary"
+                                    className="w-full rounded-[17.5px] bg-[#4D555D] mt-[20px] text-[#FFF] hover:cursor-pointer hover:!bg-[#4D555D]">
+                                立即注册
+                            </Button>
+                            <div className="w-full flex justify-center items-center flex-col mt-[20px]">
+                                <a>--更多登陆方式--</a>
+                                <img src="	https://front.cdn.xdclass.net/images/wechat_icon.webp "
+                                     className="w-[70px] h-[70px] p-[8px] my-[10px] text-center"/>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="w-full text-center bg-[#4D555D1A] h-[48px] leading-[48px]">
+                        <span>已有账号？</span>
+                        <a className="text-[#169BD5]">登录</a>
+                    </div>
+
+
+                </Col>
+
             </Row>
         </Modal>
 
